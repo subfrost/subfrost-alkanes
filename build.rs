a9cb5fde77af79ef9e77b02050c43468633871ea
kungfuflex <judoflexchop@gmail.com>
2024-12-08 17:18:36 +0000

    separate builds

---
+++ build.rs
@@ -0,0 +1,154 @@
+use anyhow::Result;
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use hex;
+use std::env;
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+fn compress(binary: Vec<u8>) -> Result<Vec<u8>> {
+    let mut writer = GzEncoder::new(Vec::<u8>::with_capacity(binary.len()), Compression::best());
+    writer.write_all(&binary)?;
+    Ok(writer.finish()?)
+}
+
+fn build_alkane(wasm_str: &str, features: Vec<&'static str>) -> Result<()> {
+    if features.len() != 0 {
+        let _ = Command::new("cargo")
+            .env("CARGO_TARGET_DIR", wasm_str)
+            .arg("build")
+            .arg("--release")
+            .arg("--features")
+            .arg(features.join(","))
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()?
+            .wait()?;
+        Ok(())
+    } else {
+        Command::new("cargo")
+            .env("CARGO_TARGET_DIR", wasm_str)
+            .arg("build")
+            .arg("--release")
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()?
+            .wait()?;
+        Ok(())
+    }
+}
+
+fn main() {
+    let env_var = env::var_os("OUT_DIR").unwrap();
+    let base_dir = Path::new(&env_var)
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap();
+    let out_dir = base_dir.join("release");
+    let wasm_dir = base_dir.parent().unwrap().join("alkanes");
+    fs::create_dir_all(&wasm_dir).unwrap();
+    let wasm_str = wasm_dir.to_str().unwrap();
+    let write_dir = Path::new(&out_dir)
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("src")
+        .join("tests");
+
+    fs::create_dir_all(&write_dir.join("std")).unwrap();
+    let crates_dir = out_dir
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("alkanes");
+    std::env::set_current_dir(&crates_dir).unwrap();
+    let mods = fs::read_dir(&crates_dir)
+        .unwrap()
+        .filter_map(|v| {
+            let name = v.ok()?.file_name().into_string().ok()?;
+            Some(name)
+        })
+        .collect::<Vec<String>>();
+    let files = mods
+        .clone()
+        .into_iter()
+        .filter_map(|name| {
+            let mut vars = env::vars_os();
+            Some(name)
+        })
+        .collect::<Vec<String>>();
+    files.into_iter()
+        .map(|v| -> Result<String> {
+            std::env::set_current_dir(&crates_dir.clone().join(v.clone()))?;
+            build_alkane(wasm_str, vec![])?;
+            std::env::set_current_dir(&crates_dir)?;
+            let subbed = v.clone().replace("-", "_");
+            eprintln!(
+                "write: {}",
+                write_dir
+                    .join("std")
+                    .join(subbed.clone() + "_build.rs")
+                    .into_os_string()
+                    .to_str()
+                    .unwrap()
+            );
+            let f: Vec<u8> = fs::read(
+                &Path::new(&wasm_str)
+                    .join("wasm32-unknown-unknown")
+                    .join("release")
+                    .join(subbed.clone() + ".wasm"),
+            )?;
+            let compressed: Vec<u8> = compress(f.clone())?;
+            fs::write(&Path::new(&wasm_str).join("wasm32-unknown-unknown").join("release").join(subbed.clone() + ".wasm.gz"), &compressed)?;
+            let data: String = hex::encode(&f);
+            fs::write(
+                &write_dir.join("std").join(subbed.clone() + "_build.rs"),
+                String::from("use hex_lit::hex;\n#[allow(long_running_const_eval)]\npub fn get_bytes() -> Vec<u8> { (&hex!(\"")
+                    + data.as_str()
+                    + "\")).to_vec() }",
+            )?;
+            eprintln!(
+                "build: {}",
+                write_dir
+                    .join("std")
+                    .join(subbed.clone() + "_build.rs")
+                    .into_os_string()
+                    .to_str()
+                    .unwrap()
+            );
+            Ok(subbed)
+        })
+        .collect::<Result<Vec<String>>>()
+        .unwrap();
+    eprintln!(
+        "write test builds to: {}",
+        write_dir
+            .join("std")
+            .join("mod.rs")
+            .into_os_string()
+            .to_str()
+            .unwrap()
+    );
+    fs::write(
+        &write_dir.join("std").join("mod.rs"),
+        mods.into_iter()
+            .map(|v| v.replace("-", "_"))
+            .fold(String::default(), |r, v| {
+                r + "pub mod " + v.as_str() + "_build;\n"
+            }),
+    )
+    .unwrap();
+}
---
+++ src/tests/fr.rs
@@ -0,0 +1,46 @@
+use alkanes::message::AlkaneMessageContext;
+use alkanes_support::cellpack::Cellpack;
+use alkanes_support::id::AlkaneId;
+use anyhow::Result;
+use bitcoin::blockdata::transaction::OutPoint;
+use metashrew_support::index_pointer::KeyValuePointer;
+use protorune::{balance_sheet::load_sheet, message::MessageContext, tables::RuneTable};
+
+use protorune_support::utils::consensus_encode;
+
+use crate::index_block;
+use crate::tests::helpers as alkane_helpers;
+use crate::tests::std::alkanes_std_genesis_alkane_build;
+#[allow(unused_imports)]
+use metashrew::{get_cache, index_pointer::IndexPointer, println, stdio::stdout};
+use alkane_helpers::clear;
+use std::fmt::Write;
+use wasm_bindgen_test::wasm_bindgen_test;
+#[wasm_bindgen_test]
+fn test_genesis() -> Result<()> {
+    clear();
+    let block_height = 850_000;
+    let cellpacks: Vec<Cellpack> = [
+        //auth token factory init
+        Cellpack {
+            target: AlkaneId { block: 3, tx: 0 },
+            inputs: vec![0],
+        },
+    ]
+    .into();
+    let test_block = alkane_helpers::init_with_multiple_cellpacks_with_tx(
+        [fr_build::get_bytes(), vec![]].into(),
+        cellpacks,
+    );
+    let len = test_block.txdata.len();
+    let outpoint = OutPoint {
+        txid: test_block.txdata[len - 1].compute_txid(),
+        vout: 0,
+    index_block(&test_block, block_height)?;
+    let ptr = RuneTable::for_protocol(AlkaneMessageContext::protocol_tag())
+        .OUTPOINT_TO_RUNES
+        .select(&consensus_encode(&outpoint)?);
+    let sheet = load_sheet(&ptr);
+    println!("balances at end {:?}", sheet);
+    Ok(())
+}
